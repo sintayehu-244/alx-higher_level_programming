f6a998ca7ea6ec62f21d6c255c82a95d5ab052be
sintayehu-244 <101401891+sintayehu-244@users.noreply.github.com>
2022-10-03 23:12:21 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
+// Prints a message depending of the number of arguments passed
 
-const len = process.argv.length;
-
-if (len < 3) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (len === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
