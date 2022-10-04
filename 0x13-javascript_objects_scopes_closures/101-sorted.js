fb79bb84d3aa3c5869c095c62c9ef76929ccd92a
sintayehu-244 <101401891+sintayehu-244@users.noreply.github.com>
2022-10-04 19:38:28 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
-  }
+const list = require('./101-data').dict;
+const sorted = {};
+
+Object.keys(list).forEach(key => {
+  if (sorted[list[key]] === undefined) sorted[list[key]] = [];
+  sorted[list[key]].push(key);
 }
-console.log(newDict);
+);
+console.log(sorted);
