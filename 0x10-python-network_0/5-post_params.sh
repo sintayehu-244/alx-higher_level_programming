46f142285079b96c48d1b40f4de65c3d1c50d647
sintayehu-244 <101401891+sintayehu-244@users.noreply.github.com>
2022-10-20 23:19:42 +0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
