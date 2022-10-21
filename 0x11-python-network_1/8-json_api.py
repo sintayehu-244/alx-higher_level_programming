5ac973a7cad50fb3815123f7f0cd37fffc52c5a1
sintayehu-244 <101401891+sintayehu-244@users.noreply.github.com>
2022-10-22 01:30:52 +0300

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 """
-Python script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter
-"""
-import requests
-from sys import argv
+Script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
 
-if __name__ == '__main__':
-    q = argv[1] if len(argv) == 2 else ""
+The letter must be sent in the variable `q`
+If no argument is given, set `q=""`
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
-    r = requests.post(url, data={'q': q})
+    if len(sys.argv) == 1:
+        letter = ""
+    else:
+        letter = sys.argv[1]
+    r = requests.post(url, data={'q': letter})
     try:
-        r_dict = r.json()
-        id, name = r_dict.get('id'), r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
-            print("No result")
+        response = r.json()
+        if len(response) == 0:
+            print('No result')
         else:
-            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
-    except:
-        print("Not a valid JSON")
+            print('[{}] {}'.format(response.get('id'), response.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
